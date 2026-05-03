4e03660970a132026180cdd9423db55b5244edf2
Amadhi Wijesinghe <amadhikaveendaya@gmail.com>
2026-05-03 17:33:58 +0530

    victus clean setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+app.get("/products", (req, res) => {
+  db.query("SELECT * FROM products", (err, result) => {
+    if (err) return res.json(err);
+    res.json(result);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running...");
+});
